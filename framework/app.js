070fb9918f5e69038c01ec0a1a6b133c2e9b5b6b
Justin Yang <justin@y4ng.com>
2014-07-20 11:23:23 -0700

    Added favicon

--- framework/app.js
+++ framework/app.js
@@ -16,7 +16,6 @@ var app = express();
 app.set('port', process.env.PORT || 1337);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
