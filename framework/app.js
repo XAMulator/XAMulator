fe7aa31fa9278b27a284d9b7668b35d3dd482510
woodenchopsticks <jonathan.z99@gmail.com>
2014-07-29 00:24:53 -0700

    REDONE APP LAYOUT - Server
    Server needs to in one app.js file

--- framework/app.js
+++ framework/app.js
@@ -52,20 +52,112 @@ app.post('/newtest', function(request, response) {
 	response.set("Access-Control-Allow-Origin", "*");
 	console.log("Connected to Database")
 	console.log(request.body);
-	var body = response.json(request.body);
-	connection.query("INSERT INTO tests VALUES (" + 
-												connection.escape(body.P_Id) + ', "' +
-												connection.escape(body.name) + '",' +
-												connection.escape(body.totalPoints) + ', \'' +
-												connection.escape(body.datetimeCreated) + '\',' +
-												connection.escape(body.datetimeTest) + ',' + 
-												connection.escape(body.testAvailable) + ',' +
-												connection.escape(body.foreignKey) + ',' +
-												connection.escape(body.randomnized) + ',"' +
-												connection.escape(body.examtime) + '")');
+	response.json(request.body); //Temporary
+	console.log(typeof request.body);
+
+	//connection.query("INSERT INTO tests VALUES (" + 
+	// 											connection.escape(body.P_Id) + ', "' +
+	// 											connection.escape(body.name) + '",' +
+	// 											connection.escape(body.totalPoints) + ', \'' +
+	// 											connection.escape(body.datetimeCreated) + '\',' +
+	// 											connection.escape(body.datetimeTest) + ',' + 
+	// 											connection.escape(body.testAvailable) + ',' +
+	// 											connection.escape(body.foreignKey) + ',' +
+	// 											connection.escape(body.randomnized) + ',"' +
+	// 											connection.escape(body.examtime) + '")');
 	connection.end();
 });
 
+app.post("/login/", function(request, response) {
+	connection.query("SELECT * FROM students WHERE username='" + connection.escape(request.body.username) + "'", function(err, rows, fields) {
+		if (rows.length > 0) {
+			if (rows[0].password == request.body.password) {
+				// send auth token????
+			}
+		} else {
+			response.send('{"error": "no such user found"}');
+		}
+	});
+	request.body.username;
+	request.body.password;
+});
+// Get test questions
+app.post("/taketest/", function(req, res) {
+	res.set("Access-Control-Allow-Origin", "*");
+	var body = req.body;
+	connection.query("SELECT * FROM tests WHERE P_Id=" + connection.escape(body), function(err, rows, fields) {
+		if (rows.length > 0) {
+			if (rows[0].testAvailable) {
+				var isRandomized = rows[0].randomnized;
+				connection.query("SELECT * FROM questions WHERE test=" + connection.escape(body), function(err, rows, field) {
+					var questionArray = [];
+					rows.forEach(function(e) {
+						questionArray.push({"question": e.questionContent, "answers": JSON.parse(e.answersJSON), "type": e.questiontype});
+					});
+					// parse test and send test here.
+					if (!isRandomized) {
+						res.send(JSON.stringify({"error": null, "test": questionArray}));
+					} else {
+						res.send(JSON.stringify({"error": null, "test": shuffle(questionArray)}));
+					}
+				});
+			} else {
+				res.send('{"error": "test not available"}');
+			}
+		} else {
+			res.send("{\"error\": \"test not found\"}");
+		}
+	});
+
+});
+// Check test status
+app.post("/checkstatus/", function(req, res) {
+	res.set("Access-Control-Allow-Origin", "*");
+	var body = req.body;
+	connection.query("SELECT * FROM tests WHERE P_Id=" + connection.escape(data) + " LIMIT 1", function(err, rows, fields) {
+		res.writeHead(200);
+		delete rows[0].P_Id;
+		delete rows[0].O_Id;
+		res.send(JSON.stringify(rows[0]));
+	});
+});
+app.post("/gradetest/", function(req, res) {
+	var body = req.body;
+	// Get answers
+	var answerArr = [];
+	connection.query("SELECT * FROM questions WHERE test=" + connection.escape(body.testId), function(err, rows, fields) {
+		rows.forEach(function(row) {
+			answerArr.push(row);
+		});
+	});
+	// Get student responses <-- implement after we manage to store student responses somehow.
+	var studentRes = ["foo", "bar"];
+	// populate array with T or F based on correct/incorrect
+	var corrections = [];
+	for (var i = 0; i < studentRes.length; i++) {
+		if (e[i] == answerArr[i].correctAnswer) {
+			corrections[i] = true;
+		} else if (e[i] == null) {
+			corrections[i] = null;
+		} else {
+			corrections[i] = false;
+		}
+	}
+	// Add up all the points and store into DB.
+	var totalPoints = 0;
+	for (var i = 0; i < corrections.length; i++) {
+		if (corrections[i]) {
+			totalPoints += answerArr[i].fullPoints;
+		} else if (corrections[i] == null) {
+			totalPoints += answerArr[i].noAnswerPoints;
+		} else {
+			totalPoints -= answerArr[i].wrongAnswerPoints;
+		}
+	}
+	// Mock storing into DB
+	/*
+	* connection.query("INSERT INTO TABLE ??? VALUES (" + connection.escape(totalPoints) + ", " + connection.escape(body.studentId) + ")");
+	*/
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
